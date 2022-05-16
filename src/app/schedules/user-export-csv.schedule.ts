eb4c8922429eb420e67e38822fb24805e110051b
Thiago <tsv.thiagosilva@gmail.com>
2022-05-16 00:23:57 -0300

    ci: include .env files to .gitignore

--- src/app/schedules/user-export-csv.schedule.ts
+++ src/app/schedules/user-export-csv.schedule.ts
@@ -3,7 +3,7 @@ import automation from '@modules/users/user-export-csv.automation'
 
 export default scheduleService({
   name: 'users-export-csv',
-  interval: { time: 5, unit: 'seconds' },
+  interval: { time: 10, unit: 'seconds' },
   callback: automation,
   options: {
     executions: 1,
--- src/app/schedules/user.schedule.ts
+++ src/app/schedules/user.schedule.ts
@@ -3,7 +3,7 @@ import automation from '@modules/users/user.automation'
 
 export default scheduleService({
   name: 'users',
-  interval: { time: 5, unit: 'seconds' },
+  interval: { time: 10, unit: 'seconds' },
   callback: automation,
   options: {
     executions: 1,
